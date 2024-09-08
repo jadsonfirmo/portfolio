26e5f09fcb8f35168f616895b36c7f8eda3b9cfb
Jadson Firmo <jadson.firmo@gmail.com>
2024-09-08 12:59:05 -0300

    Inclui rotas

--- portfolio-app/src/app/app.routes.ts
+++ portfolio-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ExperiencesComponent } from 'app/pages/experiences/experiences.component';
+import { HomeComponent } from 'app/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'experiences', component: ExperiencesComponent },
+
+  { path: '**', redirectTo: 'home' },
+];
---
+++ portfolio-app/src/app/pages/experiences/experiences.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiences.component.html',
+  styles: [],
+})
+export class ExperiencesComponent {}
