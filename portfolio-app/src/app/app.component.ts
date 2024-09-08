9c7dff3ac79c6c61e4a918474f771ed465d4d18a
Jadson Firmo <jadson.firmo@gmail.com>
2024-09-08 15:29:48 -0300

    Adicionando layout

--- portfolio-app/src/app/app.component.ts
+++ portfolio-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContentLayoutComponent } from 'app/layouts/content-layout/content-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContentLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio-app';
---
+++ portfolio-app/src/app/layouts/content-layout/content-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-layout.component.html',
+  styles: [],
+})
+export class ContentLayoutComponent {}
